4e2e50f2aae2b4a34992e6db885882c1ca6384a4
Andrzej Malik <andrzejkom2016@gmail.com>
2020-06-05 12:11:56 +0200

    nowe polecenie

--- CreditCardValid/Program.cs
+++ CreditCardValid/Program.cs
@@ -8,7 +8,7 @@ class CreditCard
     public static void Main()
     {
         long number;
-        Console.Write("Enter credit card number (only numbers not letters): ");
+        Console.Write("Enter credit card number: ");
         number = long.Parse( Console.ReadLine());
         Console.Write(number + " is " +
                      (isValid(number) ?
